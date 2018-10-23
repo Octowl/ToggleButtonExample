ccfd01bc4d781551709b227beb40529d20ca03eb
Octowl <alsaff1987@gmail.com>
2018-10-23 16:56:46 +0300

    Toggle Button example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import LightBox from "./LightBox";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  toggle() {
+    const open = !this.state.open;
+    this.setState({ open });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <LightBox open={this.state.open} />
+        <button onClick={this.toggle.bind(this)}>TOGGLE</button>
       </div>
     );
   }
